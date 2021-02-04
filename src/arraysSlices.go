679221ee4e6838e969f8af9d66ae2d113cc4fa1f
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-04 17:31:50 +0000

    Examples on using cap and len instructions for Slices

--- src/arraysSlices.go
+++ src/arraysSlices.go
@@ -6,10 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Arrays and Slices")
+
+	arraysAndSlices()
 }
 
 func arraysAndSlices() {
 
+	// By doing so an Array is created to hold the value of the Slice
+	// since the Slice holds only references to an Array
 	myCourses := make([]string, 5, 10)
+
+	fmt.Printf("Capacity: %d and Lenght %d \n",
+		cap(myCourses), len(myCourses))
 
 }

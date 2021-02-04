bf67dc37435ea9bc37cf7d2e57b9d637753aa681
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-04 21:05:52 +0000

    Declaring and initializing of Maps

--- src/maps.go
+++ src/maps.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Maps")
+
+	leagueTitles := make(map[string]int)
+	leagueTitles["Vitoria"] = 1
+	leagueTitles["Porto"] = 30
+
+	fmt.Println(leagueTitles)
+
+	premierLeagueTitles := map[string]int{
+		"ManUnited": 20,
+		"Arsenal":   5,
+	}
+
+	fmt.Println(premierLeagueTitles)
 }

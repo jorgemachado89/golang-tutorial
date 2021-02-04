9219a7dd5b236171c290e81d50fa4884ebffaa41
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-04 16:54:06 +0000

    Added for post condition and in range loops

--- src/loops.go
+++ src/loops.go
@@ -5,12 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Loops")
-	countdownTimer()
+	fmt.Println("Loops\n")
+	//countdownTimer()
+	forRangeLoops()
 }
 
 func countdownTimer() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("i: ", i)
 	}
+}
+
+func forRangeLoops() {
+	courses := []string {"Docker Deep Dive", "Docker Clustering", "Docker and Kubernetes"}
+
+	for _, course := range courses {
+		fmt.Println(course)
+	}
+
 }

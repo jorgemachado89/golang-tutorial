11f3eac005a8da7e9c74dd23def59244c56d416f
Jorge Machado (CTW) <Jorge.MB.Machado@ctw.bmwgroup.com>
2021-02-04 14:34:30 +0000

    Added examples of custom type, HOF and first class functions

--- src/variadic.go
+++ src/variadic.go
@@ -5,9 +5,15 @@ import (
 )
 
 func main() {
-	bestFinish := bestLeagueFinishes(13, 10, 13, 17, 14, 16, 7, 5, 2)
-	fmt.Println(bestFinish)
+
+	// bestFinish := bestLeagueFinishes(13, 10, 13, 17, 14, 16, 7, 5, 2)
+	// fmt.Println(bestFinish)
+
+	tailoredBestFinish := tailoredResultHoc(bestLeagueFinishes)
+	fmt.Println(tailoredBestFinish)
 }
+
+type bestLeague func(...int) int
 
 func bestLeagueFinishes(finishes ...int) int {
 	best := finishes[0]
@@ -18,4 +24,10 @@ func bestLeagueFinishes(finishes ...int) int {
 	}
 
 	return best
+}
+
+func tailoredResultHoc(positionCalc bestLeague) string {
+	leagueRanks := []int{13, 10, 13, 17, 14, 16, 7, 5, 2}
+
+	return fmt.Sprint("Highest League Finish: ", positionCalc(leagueRanks...))
 }

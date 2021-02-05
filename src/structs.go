303ea8c4a1d449c4dd7d8b583c870ab203f3ec14
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-05 17:05:15 +0000

    Add example of when to use string ptr and why

--- src/structs.go
+++ src/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -13,6 +14,48 @@ type courseMeta struct {
 func main() {
 	fmt.Println("structs")
 
+	// basicStructInit()
+
+	jsonDoc := `
+        {
+            "environment" : "asdf",
+            "hostName" : ""
+        }
+	`
+	conf := &configWithPointers{}
+	json.Unmarshal([]byte(jsonDoc), conf)
+
+	fmt.Println(jsonDoc)
+	fmt.Println(conf)
+}
+
+// Using string pointers differs from empty strings in the sense that the value might not have been initialized at all.
+type configWithPointers struct {
+	Environment *string // pointer to string
+	Version     *string
+	HostName    *string
+}
+
+func (c *configWithPointers) String() string {
+	envOut, verOut, hostOut := "<nil>", "<nil>", "<nil>"
+
+	if c.Environment != nil { // Check for nil!
+		envOut = *c.Environment
+	}
+
+	if c.Version != nil {
+		verOut = *c.Version
+	}
+
+	if c.HostName != nil {
+		hostOut = *c.HostName
+	}
+
+	return fmt.Sprintf("Environment: '%v'\nVersion:'%v'\nHostName: '%v'",
+		envOut, verOut, hostOut)
+}
+
+func basicStructInit() {
 	ThisCourse := courseMeta{
 		Author: "Someone",
 		Level:  "Aiai",
